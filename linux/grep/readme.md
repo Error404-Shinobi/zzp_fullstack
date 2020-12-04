456c99c6a492b253e50098cf6db232e30be55078
xuansa <xuansa@vip.qq.com>
2020-12-04 21:33:27 +0800

    grep小试

---
+++ linux/grep/readme.md
@@ -0,0 +1,9 @@
+- gitbash 是个linux 最小化战场
+- grep 命令 用于查找
+    grep moon monkey (在monkey中查找关于moon的记录)
+    grep -v moon monkey 查找反向条件
+    -c 显示符合条件的数量
+    -i 大小写忽略
+    -n 输出内容同时输出行号
+    ^ 匹配以xxx作为开始的记录（^J,以J为开始的记录）
+    $ 匹配以xxx作为结束的记录 (呢$)
---
+++ node/cherrio/index.js
@@ -0,0 +1,22 @@
+let request = require('node-fetch');//第三方npm
+//上条不可用时可将require中换成http这个内置库,换成node-fetch库也可
+let cheerio = require('cheerio');
+
+let url = 'https://movie.douban.com/top250';
+
+const main = async () => {//async 关键字
+    // let html = await request({//等待爬取
+    //     url
+        
+    // });
+    // console.log(html);
+    fetch(url)
+            .then(res => res.text())
+            .then(body => {
+                let $ = cheerio.load(body);
+                let movieNodes = $('#content .item');
+                console.log(movieNodes)
+            });
+    
+}
+main ();
